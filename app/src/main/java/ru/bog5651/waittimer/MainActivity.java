ed753f0ceef807a23b488774639c3764c7024e11
Evgeniy Divold <div98@mail.ru>
2023-11-05 21:05:34 +0600

    add tdlib api

--- app/src/main/java/ru/bog5651/waittimer/MainActivity.java
+++ app/src/main/java/ru/bog5651/waittimer/MainActivity.java
@@ -3,12 +3,10 @@ package ru.bog5651.waittimer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 import ru.bog5651.waittimer.databinding.ActivityMainBinding;
 
@@ -17,12 +15,15 @@ public class MainActivity extends Activity {
     private View root;
     private ActivityMainBinding binding;
     final Handler h = new Handler();
+    private TgClient client;
 
     private int seconds = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        client = new TgClient(getCacheDir());
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
@@ -35,13 +36,14 @@ public class MainActivity extends Activity {
         root.setOnClickListener(v -> {
             seconds = 30;
             timerView.setText(String.valueOf(seconds));
+            Log.d("My app", String.format("onCreate: %s", client.link));
         });
 
-        h.postDelayed(new TimerTask() {
+        h.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (seconds <= 0) {
-                    finishAffinity();
+//                    finishAffinity();
                     return;
                 }
                 runOnUiThread(() -> {
---
+++ app/src/main/java/ru/bog5651/waittimer/TgClient.java
@@ -0,0 +1,108 @@
+package ru.bog5651.waittimer;
+
+import android.util.Base64;
+import android.util.Log;
+
+import org.drinkless.td.libcore.telegram.Client;
+import org.drinkless.td.libcore.telegram.TdApi;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+
+public class TgClient {
+    private final File appDir;
+    private TdApi.AuthorizationState state;
+    private Client client;
+    public String link;
+
+    public TgClient(File appDir) {
+        this.appDir = appDir;
+        client = Client.create(object -> {
+            Log.d("My app", String.format("update handler: %s", object.toString()));
+
+            switch (object.getConstructor()) {
+                case TdApi.UpdateAuthorizationState.CONSTRUCTOR:
+                    onAuthorizationStateUpdated(((TdApi.UpdateAuthorizationState) object).authorizationState);
+                    break;
+            }
+        }, e -> {
+            Log.d("My app", String.format("update handler error %s\n", e.toString()));
+        }, e -> {
+            Log.d("My app", String.format("def handler error %s\n", e.toString()));
+        });
+
+        this.setTdlibParameters(null);
+    }
+
+    private void onAuthorizationStateUpdated(TdApi.AuthorizationState authorizationState) {
+        if (authorizationState != null) {
+            state = authorizationState;
+        }
+        switch (state.getConstructor()) {
+            case TdApi.AuthorizationStateWaitTdlibParameters.CONSTRUCTOR:
+                this.setTdlibParameters((TdApi.AuthorizationStateWaitTdlibParameters) authorizationState);
+                break;
+            case TdApi.AuthorizationStateWaitOtherDeviceConfirmation.CONSTRUCTOR:
+                link = ((TdApi.AuthorizationStateWaitOtherDeviceConfirmation) state).link;
+                break;
+            case TdApi.AuthorizationStateWaitEncryptionKey.CONSTRUCTOR:
+                this.checkDBEncriptionKey();
+                break;
+            case TdApi.AuthorizationStateClosed.CONSTRUCTOR:
+                break;
+            case TdApi.AuthorizationStateClosing.CONSTRUCTOR:
+                break;
+            case TdApi.AuthorizationStateLoggingOut.CONSTRUCTOR:
+                break;
+            case TdApi.AuthorizationStateReady.CONSTRUCTOR:
+                break;
+            case TdApi.AuthorizationStateWaitCode.CONSTRUCTOR:
+                break;
+            case TdApi.AuthorizationStateWaitPassword.CONSTRUCTOR:
+                break;
+            case TdApi.AuthorizationStateWaitPhoneNumber.CONSTRUCTOR:
+                this.switchToOtherDeviseConfirmation();
+                break;
+            case TdApi.AuthorizationStateWaitRegistration.CONSTRUCTOR:
+                break;
+        }
+    }
+
+    private void switchToOtherDeviseConfirmation() {
+        TdApi.RequestQrCodeAuthentication request = new TdApi.RequestQrCodeAuthentication();
+        client.send(request, null);
+    }
+
+    private void setTdlibParameters(TdApi.AuthorizationStateWaitTdlibParameters authorizationStateWaitTdlibParameters) {
+        TdApi.TdlibParameters parameters = new TdApi.TdlibParameters();
+        TdApi.SetTdlibParameters request = new TdApi.SetTdlibParameters(parameters);
+        parameters.databaseDirectory = appDir.getAbsolutePath() + "/tdlib/db";
+        parameters.useTestDc = false;
+        parameters.useMessageDatabase = false;
+        parameters.useSecretChats = false;
+        parameters.useFileDatabase = true;
+        parameters.apiId = 1;
+        parameters.apiHash = "none";
+        parameters.systemLanguageCode = "ru";
+        parameters.deviceModel = "WearOs";
+        parameters.applicationVersion = "1.0";
+        parameters.enableStorageOptimizer = true;
+
+        client.send(request, e -> {
+            Log.d("My app", String.format("setTdlibParameters handler error %s\n", e.toString()));
+        });
+    }
+
+    private void checkDBEncriptionKey() {
+        String key = "mysuperenckey";
+        byte[] data = null;
+        try {
+            data = key.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        TdApi.CheckDatabaseEncryptionKey request = new TdApi.CheckDatabaseEncryptionKey(data);
+        client.send(request, null);
+    }
+}
